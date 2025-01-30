022406d6f80c9e7ee14278a4689436d58d30126f
maxulysse <max.u.garcia@gmail.com>
2025-01-30 15:21:23 +0100

    Update documentation in README, and nicely reorder all references yaml files

--- bin/generate_references_yaml.sh
+++ bin/generate_references_yaml.sh
@@ -64,8 +64,9 @@ echo "Number of files in manifest: $total_files"
 
 cp manifest.txt leftover_manifest.txt
 
-# Remove existing assets
+# Remove existing references yaml files
 rm -rf igenomes/
+rm -rf igenomes_no_base/
 
 # Generate base info in species/genome/build.yml
 
@@ -75,7 +76,7 @@ rm -rf igenomes/
 
 cat manifest.txt | grep "\.fai" | grep -v "Bowtie2Index" | grep -v "fai\.gz" > tmp_fai.txt
 
-echo "Populating assets for fasta and fai"
+echo "Populating references yaml files for fasta and fai"
 
 for i in `cat tmp_fai.txt`;
 do
@@ -110,7 +111,7 @@ done
 # All source README
 cat manifest.txt | grep "README" | grep -v "Archives" | grep -v "beagle" | grep -v "plink" | grep -v "PhiX\/Illumina\/RTA\/Annotation\/README\.txt" > tmp_readme.txt
 
-echo "Populating assets for README"
+echo "Populating references yaml files for README"
 
 for i in `cat tmp_readme.txt`;
 do
@@ -135,7 +136,7 @@ done
 # All source gtf (removing the onces coming from gencode)
 cat manifest.txt | grep "\.gtf" | grep -v "gtf\." | grep -v "STARIndex" | grep -v "Genes\.gencode" > tmp_gtf.txt
 
-echo "Populating assets for GTF"
+echo "Populating references yaml files for GTF"
 
 for i in `cat tmp_gtf.txt`;
 do
@@ -160,7 +161,7 @@ done
 # All source fasta.dict
 cat manifest.txt | grep "\.dict" | grep -v "dict\.gz" | grep -v "dict\.old" > tmp_dict.txt
 
-echo "Populating assets for fasta.dict"
+echo "Populating references yaml files for fasta.dict"
 
 for i in `cat tmp_dict.txt`;
 do
@@ -185,7 +186,7 @@ done
 # All source genes.bed
 cat manifest.txt | grep "genes\.bed" > tmp_bed.txt
 
-echo "Populating assets for genes.bed"
+echo "Populating references yaml files for genes.bed"
 
 for i in `cat tmp_bed.txt`;
 do
@@ -210,7 +211,7 @@ done
 # All source BowtieIndex
 cat manifest.txt | grep "BowtieIndex" | grep -v "MDSBowtieIndex" | rev | cut -d "/" -f 2- | rev | sort -u > tmp_bowtie.txt
 
-echo "Populating assets for BowtieIndex"
+echo "Populating references yaml files for BowtieIndex"
 
 for i in `cat tmp_bowtie.txt`;
 do
@@ -235,7 +236,7 @@ done
 # All source Bowtie2Index
 cat manifest.txt | grep "Bowtie2Index" | rev | cut -d "/" -f 2- | rev | sort -u > tmp_bowtie2.txt
 
-echo "Populating assets for Bowtie2Index"
+echo "Populating references yaml files for Bowtie2Index"
 
 for i in `cat tmp_bowtie2.txt`;
 do
@@ -260,7 +261,7 @@ done
 # All source BWAIndex (we have version0.6.0, version0.5.x, and no version specified)
 cat manifest.txt | grep "BWAIndex" | rev | cut -d "/" -f 2- | rev | sort -u > tmp_bwaindex.txt
 
-echo "Populating assets for BWAIndex"
+echo "Populating references yaml files for BWAIndex"
 
 for i in `cat tmp_bwaindex.txt`;
 do
@@ -288,7 +289,7 @@ done
 # All source BWAmem2mem
 cat manifest.txt | grep "BWAmem2Index" | rev | cut -d "/" -f 2- | rev | sort -u > tmp_bwamem2mem.txt
 
-echo "Populating assets for BWAmem2Index"
+echo "Populating references yaml files for BWAmem2Index"
 
 for i in `cat tmp_bwamem2mem.txt`;
 do
@@ -313,7 +314,7 @@ done
 # All source Dragmap
 cat manifest.txt | grep "dragmap" | rev | cut -d "/" -f 2- | rev | sort -u > tmp_dragmap.txt
 
-echo "Populating assets for DragmapHashtable"
+echo "Populating references yaml files for DragmapHashtable"
 
 for i in `cat tmp_dragmap.txt`;
 do
@@ -338,7 +339,7 @@ done
 # All source BismarkIndex
 cat manifest.txt | grep "BismarkIndex\/genome\.fa" | rev | cut -d "/" -f 2- | rev | sort -u > tmp_bismark.txt
 
-echo "Populating assets for BismarkIndex"
+echo "Populating references yaml files for BismarkIndex"
 
 for i in `cat tmp_bismark.txt`;
 do
@@ -363,7 +364,7 @@ done
 # All source star Index
 cat manifest.txt | grep "STARIndex" | rev | cut -d "/" -f 2- | rev | sort -u > tmp_star.txt
 
-echo "Populating assets for STARIndex"
+echo "Populating references yaml files for STARIndex"
 
 for i in `cat tmp_star.txt`;
 do
@@ -388,7 +389,7 @@ done
 # All source Chromosomes fasta
 cat manifest.txt | grep "Chromosomes" | rev | cut -d "/" -f 2- | rev | sort -u > tmp_chromosomes.txt
 
-echo "Populating assets for Chromosomes fasta"
+echo "Populating references yaml files for Chromosomes fasta"
 
 for i in `cat tmp_chromosomes.txt`;
 do
@@ -413,7 +414,7 @@ done
 # All source AbundantSequences fasta
 cat manifest.txt | grep "AbundantSequences" | rev | cut -d "/" -f 2- | rev | sort -u > tmp_abundantsequences.txt
 
-echo "Populating assets for AbundantSequences fasta"
+echo "Populating references yaml files for AbundantSequences fasta"
 
 for i in `cat tmp_abundantsequences.txt`;
 do
@@ -438,7 +439,7 @@ done
 # All source refFlat (removing the ones coming from gencode)
 cat manifest.txt | grep "refFlat\.txt" | grep -v "\.gz\.bak" | grep -v "Genes\.gencode" > tmp_refflat.txt
 
-echo "Populating assets for refFlat"
+echo "Populating references yaml files for refFlat"
 
 for i in `cat tmp_refflat.txt`;
 do
@@ -463,7 +464,7 @@ done
 # All source refgene
 cat manifest.txt | grep "refGene\.txt" | grep -v "Archives" | grep -v "\.gz\.bak" > tmp_refgene.txt
 
-echo "Populating assets for refgene"
+echo "Populating references yaml files for refgene"
 
 for i in `cat tmp_refgene.txt`;
 do
@@ -488,7 +489,7 @@ done
 # All source ChromInfo.txt
 cat manifest.txt | grep "ChromInfo\.txt" | grep -v "Archives" > tmp_chrominfo.txt
 
-echo "Populating assets for ChromInfo.txt"
+echo "Populating references yaml files for ChromInfo.txt"
 
 for i in `cat tmp_chrominfo.txt`;
 do
@@ -513,7 +514,7 @@ done
 # All source GenomeSize.xml (removing the old ones)
 cat manifest.txt | grep "GenomeSize\.xml" | grep -v "\.old" > tmp_GenomeSize.txt
 
-echo "Populating assets for GenomeSize.xml"
+echo "Populating references yaml files for GenomeSize.xml"
 
 for i in `cat tmp_GenomeSize.txt`;
 do
@@ -538,7 +539,7 @@ done
 # All source hairpin.fa
 cat manifest.txt | grep "SmallRNA\/hairpin\.fa" > tmp_hairpin.txt
 
-echo "Populating assets for hairpin.fa"
+echo "Populating references yaml files for hairpin.fa"
 
 for i in `cat tmp_hairpin.txt`;
 do
@@ -563,7 +564,7 @@ done
 # All source mature.fa
 cat manifest.txt | grep "SmallRNA\/mature\.fa" > tmp_mature.txt
 
-echo "Populating assets for mature.fa"
+echo "Populating references yaml files for mature.fa"
 
 for i in `cat tmp_mature.txt`;
 do
@@ -588,7 +589,7 @@ done
 # All source vcf
 cat manifest.txt | grep "\.vcf" | grep -v "\.idx" | grep -v "\.tbi" | grep -v "\.md5" > tmp_vcf.txt
 
-echo "Populating assets for vcf"
+echo "Populating references yaml files for vcf"
 
 a_dbsnp=("s3://ngi-igenomes/igenomes/Homo_sapiens/GATK/GRCh37/Annotation/GATKBundle/dbsnp_138.b37.vcf.gz"
     "s3://ngi-igenomes/igenomes/Homo_sapiens/GATK/GRCh38/Annotation/GATKBundle/beta/Homo_sapiens_assembly38.dbsnp.vcf.gz"
@@ -766,9 +767,9 @@ echo "Fixing /GATK/GRCh37.yml name to GATK/GRCh37decoy.yml"
 #  GATK/GRCh37.yml should actually be GATK/GRCh37decoy.yml
 mv igenomes/GATK/GRCh37.yml igenomes/GATK/GRCh37decoy.yml
 
-echo "Deleting assets from manifest"
+echo "Deleting references yaml files from manifest"
 
-# Delete all the lines that are used to generate the assets
+# Delete all the lines that are used to generate the references yaml files
 for i in `cat tmp_*.txt`;
 do
     sed -i "\|${i}|d" leftover_manifest.txt
@@ -783,3 +784,13 @@ echo "Number of files removed: $removed_files"
 echo "Deleting tmp files"
 
 rm -rf tmp_*
+
+cp -r igenomes igenomes_no_base
+
+echo "Generating references yaml files with igenomes_base params"
+for file in `find igenomes/ -type f`; do
+    if [[ -f $file ]]; then
+        sed -i '1s|^|# params.igenomes_base = s3://ngi-igenomes/igenomes/\n|' "$file"
+        sed -i 's|s3://ngi-igenomes/igenomes|${params.igenomes_base}|g' "$file"
+    fi
+done
